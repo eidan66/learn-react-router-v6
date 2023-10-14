758e0627a794e477ce8139bebe28b1ba80e850f4
Idan Levian <eidan47@gmail.com>
2023-10-14 13:16:02 +0300

    Adding error element

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom';
+import {Error} from './components/Error.tsx'
 
 import {About} from './pages/About/About.tsx';
 import {HostVanDetail} from './pages/Host/HostVanDetail.tsx'
@@ -7,7 +8,7 @@ import {HostVanPhotos} from './pages/Host/HostVanPhotos.tsx'
 import {HostVanPricing} from './pages/Host/HostVanPricing.tsx'
 import {HostVans} from './pages/Host/HostVans.tsx'
 import {NotFound} from './pages/NotFound.tsx'
-import {Vans} from './pages/Vans/Vans.tsx';
+import {loader as vansLoader, Vans} from './pages/Vans/Vans.tsx';
 import {Home} from './pages/Home/Home.tsx';
 
 import './server'
@@ -18,31 +19,32 @@ import {Reviews} from './pages/Host/Reviews.tsx';
 import {HostLayout} from './components/HostLayout/HostLayout.tsx';
 import {Dashboard} from './pages/Host/Dashboard.tsx';
 
-const App = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Layout/>}>
-                <Route index element={<Home/>}/>
-                <Route path="about" element={<About/>}/>
 
-                <Route path="vans" element={<Vans/>}/>
-                <Route path="vans/:id" element={<VanDetail/>}/>
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
 
-                <Route path="host" element={<HostLayout/>}>
-                    <Route index element={<Dashboard/>}/>
-                    <Route path="income" element={<Income/>}/>
-                    <Route path="reviews" element={<Reviews/>}/>
-                    <Route path="vans" element={<HostVans/>}/>
-                    <Route path="vans/:id" element={<HostVanDetail/>}>
-                        <Route index element={<HostVanInfo/>}/>
-                        <Route path="pricing" element={<HostVanPricing/>}/>
-                        <Route path="photos" element={<HostVanPhotos/>}/>
-                    </Route>
-                </Route>
-                <Route path="*" element={<NotFound/>}/>
+        <Route path="vans" element={<Vans/>} loader={vansLoader} errorElement={<Error/>}/>
+        <Route path="vans/:id" element={<VanDetail/>}/>
+
+        <Route path="host" element={<HostLayout/>}>
+            <Route index element={<Dashboard/>}/>
+            <Route path="income" element={<Income/>}/>
+            <Route path="reviews" element={<Reviews/>}/>
+            <Route path="vans" element={<HostVans/>}/>
+            <Route path="vans/:id" element={<HostVanDetail/>}>
+                <Route index element={<HostVanInfo/>}/>
+                <Route path="pricing" element={<HostVanPricing/>}/>
+                <Route path="photos" element={<HostVanPhotos/>}/>
             </Route>
-        </Routes>
-    </BrowserRouter>
+        </Route>
+        <Route path="*" element={<NotFound/>}/>
+    </Route>
+))
+
+const App = () => (
+    <RouterProvider router={router}/>
 );
 
 
